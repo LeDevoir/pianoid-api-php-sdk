de277236a23228f72e95e8de2849b4868b465f8d
Philippe Bourdeau <philippe@cloudhelp.ca>
2023-09-15 15:42:26 -0400

    Add refresh token request (#1)
    Co-authored-by: pbourdeau <pbourdeau@ledevoir.com>

--- tests/GenerateTokenTest.php
+++ tests/GenerateTokenTest.php
@@ -40,5 +40,9 @@ class GenerateTokenTest extends TestCase
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
             $pianoIdResponse->getAccessToken()
         );
+        self::assertEquals(
+            'RFGQfHHGrpddt2',
+            $pianoIdResponse->getRefreshToken()
+        );
     }
 }
